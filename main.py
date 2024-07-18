df5123e3742357fc861d76a4a5a61dca927dac31
Screamgxne <74162001+GreyKxtx@users.noreply.github.com>
2024-07-18 14:54:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class EbayScraper:
     def fetch_page(self):
         try:
             page = requests.get(self.url)
-            page.raise_for_status()  # Raise an HTTPError for bad responses
+            page.raise_for_status()
             return page.text
         except requests.RequestException as e:
             self.logger.error(f"Failed to fetch page: {e}")
@@ -24,9 +24,10 @@ class EbayScraper:
     def parse_title(self, soup):
         title_element = soup.select_one('.x-item-title__mainTitle span.ux-textspans--BOLD')
         if title_element:
-            self.item['Name'] = title_element.text.strip()
+            self.item['name'] = title_element.text.strip()
         else:
             self.logger.warning("Title element not found")
+            self.item['name'] = 'N/A'
 
     def parse_price(self, soup):
         price_html_element = soup.select_one('.x-bin-price__content .x-price-primary span.ux-textspans')
@@ -34,39 +35,36 @@ class EbayScraper:
             price_text = price_html_element.text.strip()
             match = re.search(r'[\d.,]+', price_text)
             if match:
-                self.item['Price'] = match.group()
+                self.item['price'] = match.group()
             else:
-                self.item['Price'] = 'N/A'
+                self.item['price'] = 'N/A'
         else:
             self.logger.warning("Price element not found")
+            self.item['price'] = 'N/A'
 
     def parse_photo_url(self, soup):
         image_element = soup.select_one('.ux-image-carousel-item.active img')
         if image_element:
-            self.item['Photo URL'] = image_element['src']
+            self.item['photoUrl'] = image_element['src']
         else:
             self.logger.warning("Image element not found")
+            self.item['photoUrl'] = 'N/A'
 
     def parse_shipping(self, soup):
         shipping_price_element = soup.select_one('.ux-layout-section--shipping .ux-textspans--BOLD')
         if shipping_price_element:
             shipping_price_text = shipping_price_element.text.strip()
             if "Does not ship" in shipping_price_text:
-                self.item['Shipping Price'] = f'N/A ({shipping_price_text})'
+                self.item['shippingPrice'] = f'({shipping_price_text})'
             else:
                 match = re.search(r'[\d.,]+', shipping_price_text)
                 if match:
-                    self.item['Shipping Price'] = match.group()
+                    self.item['shippingPrice'] = match.group()
                 else:
-                    self.item['Shipping Price'] = 'N/A'
+                    self.item['shippingPrice'] = 'N/A'
         else:
-            self.logger.warning("Shipping price element not found.")
-            no_shipping_message_element = soup.select_one('.ux-layout-section--shipping .ux-textspans--BOLD.ux-textspans--NEGATIVE')
-            if no_shipping_message_element:
-                no_shipping_message_text = no_shipping_message_element.text.strip()
-                self.item['Shipping Price'] = f'N/A ({no_shipping_message_text})'
-            else:
-                self.item['Shipping Price'] = 'N/A'
+            self.logger.warning("Shipping price element not found")
+            self.item['shippingPrice'] = 'N/A'
 
     def parse_details(self, soup):
         section_title_elements = soup.select('.section-title')
@@ -79,15 +77,17 @@ class EbayScraper:
                     col_value = section_col.select_one('.ux-labels-values__values')
                     if col_label and col_value:
                         details.append([col_label.text.strip(), col_value.text.strip()])
-        self.item['Details'] = details
+        self.item['details'] = details if details else 'N/A'
 
     def parse_seller_info(self, soup):
         seller_element = soup.select_one('.x-sellercard-atf__info__about-seller a.ux-action')
         if seller_element:
-            self.seller['Name'] = seller_element.text.strip()
-            self.seller['Items Link'] = seller_element['href']
+            self.seller['name'] = seller_element.text.strip()
+            self.seller['itemsLink'] = seller_element['href']
         else:
             self.logger.warning("Seller information not found")
+            self.seller['name'] = 'N/A'
+            self.seller['itemsLink'] = 'N/A'
 
     def scrape_and_save(self):
         page_content = self.fetch_page()
@@ -96,6 +96,8 @@ class EbayScraper:
             return
 
         soup = BeautifulSoup(page_content, 'html.parser')
+
+        self.item['url'] = self.url
 
         self.parse_title(soup)
         self.parse_price(soup)
@@ -106,31 +108,35 @@ class EbayScraper:
 
         self.print_item_info()
 
-        with open('product_info.json', 'w') as file:
-            json.dump({'Main Information': self.item, 'Seller Information': self.seller}, file, indent=4)
-        print("Data has been saved to 'product_info.json'")
+        with open('productInfo.json', 'w') as file:
+            json.dump({'mainInformation': self.item, 'sellerInformation': self.seller}, file, indent=4)
+        print("Data has been saved to 'productInfo.json'")
 
     def print_item_info(self):
         print("\nMain Information:")
-        main_data = [['Name', self.item.get('Name', 'N/A')],
-                     ['Price', self.item.get('Price', 'N/A')],
-                     ['Shipping Price', self.item.get('Shipping Price', 'N/A')],
-                     ['Photo URL', self.item.get('Photo URL', 'N/A')]]
+        main_data = [['Name', self.item.get('name', 'N/A')],
+                     ['Price', self.item.get('price', 'N/A')],
+                     ['Shipping Price', self.item.get('shippingPrice', 'N/A')],
+                     ['Photo URL', self.item.get('photoUrl', 'N/A')],
+                     ['URL', self.item.get('url', 'N/A')]]
 
         print(tabulate(main_data, tablefmt='grid'))
 
-        if 'Details' in self.item:
+        if 'details' in self.item:
             print("\nAbout this Item:")
             details_headers = ['Attribute', 'Value']
-            details_data = self.item['Details']
-            print(tabulate(details_data, headers=details_headers, tablefmt='grid'))
+            details_data = self.item['details']
+            if details_data != 'N/A':
+                print(tabulate(details_data, headers=details_headers, tablefmt='grid'))
+            else:
+                print("No details available for this item.")
         else:
             print("\nNo details available for this item.")
 
         if self.seller:
             print("\nSeller Information:")
-            seller_data = [['Name', self.seller.get('Name', 'N/A')],
-                           ['Items Link', self.seller.get('Items Link', 'N/A')]]
+            seller_data = [['Name', self.seller.get('name', 'N/A')],
+                           ['Items Link', self.seller.get('itemsLink', 'N/A')]]
             print(tabulate(seller_data, tablefmt='grid'))
         else:
             print("\nSeller Information not available.")
@@ -141,4 +147,3 @@ if __name__ == "__main__":
     full_url = ebay_itm_url + item_id
     scraper = EbayScraper(full_url)
     scraper.scrape_and_save()
-
